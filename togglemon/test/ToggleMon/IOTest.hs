e7a1f2a752dbdd39339186440bdd47fa94068c96
Fernando Freire <dogonthehorizon@gmail.com>
2019-08-28 06:45:28 -0700

    Add stub tests for remaining IO functions

--- togglemon/test/ToggleMon/IOTest.hs
+++ togglemon/test/ToggleMon/IOTest.hs
@@ -3,7 +3,7 @@ module ToggleMon.IOTest where
 import           Control.Exception (Exception, throw)
 import qualified Data.Text         as T
 import           Test.Tasty        (testGroup)
-import           Test.Tasty.HUnit  (testCase, (@?=))
+import           Test.Tasty.HUnit  (testCase, (@?=), assertFailure)
 import           TestUtils
 import qualified ToggleMon.IO      as ToggleIO
 import           ToggleMon.Monad
@@ -18,10 +18,17 @@ simpleListDir :: ListDirectoryAction
 simpleListDir "happy" = return simpleDirContents
 simpleListDir _       = throw SimpleException
 
-test_listDirectory = testGroup
+test_module = testGroup "ToggleMon.IO" [
+    ToggleMon.IOTest.listDirectory,
+    filterDirectory,
+    ToggleMon.IOTest.readFile,
+    exec
+  ]
+
+listDirectory = testGroup
     "listDirectory"
     [ testCase
-            "should return the contents of the given directory using the provided env function"
+            "should return the contents of the given directory"
         $ do
               result <- runTestMonad'
                   (mockEnv { envListDirFn = simpleListDir })
@@ -33,3 +40,20 @@ test_listDirectory = testGroup
             (ToggleIO.listDirectory "sad")
         result @?= Nothing
     ]
+
+filterDirectory = testGroup "filterDirectory" [
+    testCase "should only return directories" $ assertFailure "TODO",
+    testCase "should handle IO exceptions" $ assertFailure "TODO"
+  ]
+
+readFile = testGroup "readFile" [
+    testCase "should return file contents" $ assertFailure "TODO",
+    testCase "should remove leading/trailing whitespace" $ assertFailure "TODO",
+    testCase "should handle IO exceptions" $ assertFailure "TODO"
+  ]
+
+exec = testGroup "exec" [
+    testCase "should execute the given  command" $ assertFailure "TODO",
+    testCase "should handle missing command arguments" $ assertFailure "TODO",
+    testCase "should handle IO exceptions" $ assertFailure "TODO"
+  ]
