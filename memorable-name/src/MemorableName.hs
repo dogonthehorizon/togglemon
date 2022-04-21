ee1e41e7d1c8d8c2c46bc9f9e06a0c9b882de96d
Fernando Freire <dogonthehorizon@gmail.com>
2022-04-21 08:22:50 -0700

    feat(mm): add more adjectives and nouns
    Also switch to conventional commit style messages. Also use ormolu
    formatter in subproject.

--- memorable-name/src/MemorableName.hs
+++ memorable-name/src/MemorableName.hs
@@ -1,11 +1,18 @@
-module MemorableName (MemorableName(..), memorableName, getRandUpTo, nouns, adjectives) where
+module MemorableName
+  ( MemorableName (..),
+    memorableName,
+    getRandUpTo,
+    nouns,
+    adjectives,
+  )
+where
 
 import Control.Monad.Trans (MonadIO, liftIO)
-import System.Random       (getStdRandom, randomR)
+import System.Random (getStdRandom, randomR)
 
 -- | Representation of a memorable name.
-data MemorableName =
-  MemorableName String String
+data MemorableName
+  = MemorableName String String
 
 instance Show MemorableName where
   show (MemorableName a n) = a ++ "-" ++ n
@@ -13,15 +20,15 @@ instance Show MemorableName where
 -- | Get a memorable name!
 memorableName :: MonadIO m => m MemorableName
 memorableName = do
-    aIdx <- liftIO $ getRandUpTo (length adjectives)
-    nIdx <- liftIO $ getRandUpTo (length nouns)
-    return $ MemorableName (adjectives !! aIdx) (nouns !! nIdx)
+  aIdx <- liftIO $ getRandUpTo (length adjectives)
+  nIdx <- liftIO $ getRandUpTo (length nouns)
+  return $ MemorableName (adjectives !! aIdx) (nouns !! nIdx)
 
 -- | Get a random integer between 0 and the provided limit, exclusive.
 getRandUpTo :: MonadIO m => Int -> m Int
 getRandUpTo n
-    | n <= 0    = return 0
-    | otherwise = liftIO $ getStdRandom (randomR (0, n - 1))
+  | n <= 0 = return 0
+  | otherwise = liftIO $ getStdRandom (randomR (0, n - 1))
 
 nouns :: [String]
 nouns = officeSupplies
@@ -31,37 +38,55 @@ adjectives = animalAdjs
 
 officeSupplies :: [String]
 officeSupplies =
-    [ "bulletin"
-    , "calculator"
-    , "chair"
-    , "computer"
-    , "desk"
-    , "eraser"
-    , "fridge"
-    , "paper"
-    , "pen"
-    , "pencil"
-    , "plant"
-    , "printer"
-    , "scanner"
-    , "staple"
-    , "stapler"
-    ]
+  [ "bulletin",
+    "cabinet",
+    "calculator",
+    "chair",
+    "computer",
+    "desk",
+    "eraser",
+    "fridge",
+    "paper",
+    "pen",
+    "pencil",
+    "phone",
+    "plant",
+    "printer",
+    "router",
+    "scanner",
+    "sofa",
+    "staple",
+    "stapler",
+    "stationary",
+    "table"
+  ]
 
 animalAdjs :: [String]
 animalAdjs =
-    [ "aquiline"
-    , "bovine"
-    , "cervine"
-    , "corvine"
-    , "elephantine"
-    , "equine"
-    , "hircine"
-    , "hominine"
-    , "leonine"
-    , "lupine"
-    , "piscine"
-    , "serpentine"
-    , "ursine"
-    , "vulpine"
-    ]
+  [ "anserine",
+    "aquiline",
+    "avine",
+    "bovine",
+    "cameline",
+    "canine",
+    "cervine",
+    "corvine",
+    "crocodiline",
+    "elephantine",
+    "equine",
+    "feline",
+    "hircine",
+    "hominine",
+    "leonine",
+    "lupine",
+    "pavonine",
+    "piscine",
+    "porcine",
+    "serpentine",
+    "taurine",
+    "tigrine",
+    "ursine",
+    "vespine",
+    "vulpine",
+    "zebrine"
+  ]
--- memorable-name/test/MemorableNameTest.hs
+++ memorable-name/test/MemorableNameTest.hs
@@ -1,30 +1,29 @@
 module MemorableNameTest where
 
-import Control.Monad                        (replicateM_)
-import Test.SmallCheck                      (monadic)
-import Test.SmallCheck.Series.MemorableName ()
-import Test.Tasty.HUnit                     (assertBool, testCase)
-import Test.Tasty.SmallCheck                (testProperty)
-
+import Control.Monad (replicateM_)
 import MemorableName
+import Test.SmallCheck (monadic)
+import Test.SmallCheck.Series.MemorableName ()
+import Test.Tasty.HUnit (assertBool, testCase)
+import Test.Tasty.SmallCheck (testProperty)
 
 test_show =
-    testProperty "MemorableName should stringify as 'word-word'"
-        $ \(name :: MemorableName) -> case name of
-              (MemorableName adj n) -> (adj <> "-" <> n) == show name
+  testProperty "MemorableName should stringify as 'word-word'" $
+    \(name :: MemorableName) -> case name of
+      (MemorableName adj n) -> (adj <> "-" <> n) == show name
 
 test_randInt =
-    testProperty "getRandUpTo should return a number between 0 and n-1"
-        $ \(n :: Int) -> monadic $ do
-              rando <- getRandUpTo n :: IO Int
-              return $ (rando == 0 && n <= 0) || ((rando >= 0) && (rando < n))
+  testProperty "getRandUpTo should return a number between 0 and n-1" $
+    \(n :: Int) -> monadic $ do
+      rando <- getRandUpTo n :: IO Int
+      return $ (rando == 0 && n <= 0) || ((rando >= 0) && (rando < n))
 
-test_memorableName
-    = testCase "memorableName should return a memorable name"
-        $ replicateM_ 100
-        $ do
-              (MemorableName adj n) <- memorableName
-              assertBool
-                      "Generated a memorable name that wasn't in the default list of nouns or adjectives"
-                  $  (adj `elem` adjectives)
-                  && (n `elem` nouns)
+test_memorableName =
+  testCase "memorableName should return a memorable name" $
+    replicateM_ 100 $
+      do
+        (MemorableName adj n) <- memorableName
+        assertBool
+          "Generated a memorable name that wasn't in the default list of nouns or adjectives"
+          $ (adj `elem` adjectives)
+            && (n `elem` nouns)
